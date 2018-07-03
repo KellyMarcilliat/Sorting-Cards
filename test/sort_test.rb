410f636662106b9afcf16f222b3a3345251e3a09
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-03 13:55:45 -0600

    WIP - sort.rb and sort_test.rb files added

---
+++ test/sort_test.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/guess'
+require './lib/deck'
+require './lib/round'
+require './lib/sort'
+
+class SortTest < MiniTest::Test
+end
