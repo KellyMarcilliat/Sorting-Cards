3fe78984b76f231991ea96717dada8b08a270328
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-02 20:19:43 -0600

    WIP - adapted record_guess method to accept has as argument.

--- test/round_test.rb
+++ test/round_test.rb
@@ -52,73 +52,43 @@ class RoundTest < MiniTest::Test
     round = Round.new(deck)
     round.current_card
 
-    assert_instance_of Guess, round.record_guess("3 of Hearts")
+    assert_instance_of Guess, round.record_guess({value: "3", suit: "Hearts"})
   end
 
-  def test_it_adds_to_guesses_array
-    card_1 = Card.new("3", "Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    round.current_card
-    round.record_guess("3 of Hearts")
+  # def test_it_adds_to_guesses_array
+  #   card_1 = Card.new("3", "Hearts")
+  #   card_2 = Card.new("4", "Clubs")
+  #   deck = Deck.new([card_1, card_2])
+  #   round = Round.new(deck)
+  #   round.current_card
+  #   round.record_guess({value: "3", suit: "Hearts"})
+  #
+  #   assert_equal 1, round.guesses.count
+  # end
 
-    assert_equal 1, round.guesses.count
-  end
+  # def test_guess_triggers_feedback
+  #   card_1 = Card.new("3", "Hearts")
+  #   card_2 = Card.new("4", "Clubs")
+  #   deck = Deck.new([card_1, card_2])
+  #   round = Round.new(deck)
+  #   round.current_card
+  #   round.record_guess({value: "3", suit: "Hearts"})
+  #
+  #   assert_equal "Correct!", round.guesses.first.feedback
+  # end
 
-  def test_guess_triggers_feedback
-    card_1 = Card.new("3", "Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    round.current_card
-    round.record_guess("3 of Hearts")
-
-    assert_equal "Correct!", round.guesses.first.feedback
-  end
-
-  def test_it_adds_to_number_correct
-    card_1 = Card.new("3", "Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    round.current_card
-    round.record_guess("3 of Hearts")
-    round.guesses.first.feedback
-
-    assert_equal 1, round.number_correct
-  end
+  # def test_it_adds_to_number_correct
+  #   card_1 = Card.new("3", "Hearts")
+  #   card_2 = Card.new("4", "Clubs")
+  #   deck = Deck.new([card_1, card_2])
+  #   round = Round.new(deck)
+  #   round.current_card
+  #   round.record_guess({value: "3", suit: "Hearts"})
+  #   round.guesses.first.correct?
+  #   round.add_to_number_correct
+  #
+  #   assert_equal 1, round.number_correct
+  # end
 
 
 end
-
-# card_1 = Card.new("3","Hearts")
-# card_2 = Card.new("4", "Clubs")
-# deck = Deck.new([card_1, card_2])
-# round = Round.new(deck)
-# round.deck
-# => #<Deck:0x007ffdf181b9c8 @cards=[...]>
-# round.guesses
-# => []
-# round.current_card
-# => #<Card:0x007ffdf1820a90 @value="3", @suit="Hearts">
-# round.record_guess("3 of Hearts")
-# => #<Guess:0x007ffdf19c8a00 @card=#<Card:0x007ffdf1820a90 @value="3", @suit="Hearts">, @response="3 of Hearts">
-# round.guesses.count
-# => 1
-# round.guesses.first.feedback
-# => "Correct!"
-# round.number_correct
-# => 1
-# round.current_card
-# => #<Card:0x007ffdf1820a90 @value="4", @suit="Clubs">
-# round.record_guess("Jack of Diamonds")
-# => #<Guess:0x007ffdf19c8a00 @card=#<Card:0x007ffdf1820a90 @value="4", @suit="Clubs">, @response="Jack of Diamonds">
-# round.guesses.count
-# => 2
-# round.guesses.last.feedback
-# => "Incorrect."
-# round.number_correct
-# => 1
-# round.percent_correct
-# => 50
