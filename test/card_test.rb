dfddf2d0c1203817d74a2dc1e5b56739c54ee03d
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-04 17:34:42 -0600

    WIP - added integration and unit testing for set_numeric

--- test/card_test.rb
+++ test/card_test.rb
@@ -10,4 +10,36 @@ class CardTest < MiniTest::Test
 
     assert_instance_of Card, card
   end
+
+  def test_it_sets_tens_digit
+    card_1 = Card.new("5", "Diamonds")
+
+    assert_equal 50, card_1.set_tens_in_numeric
+
+    card_2 = Card.new("10", "Hearts")
+
+    assert_equal 100, card_2.set_tens_in_numeric
+  end
+
+  def test_it_sets_ones_digit
+    card_1 = Card.new("5", "Diamonds")
+    card_1.set_tens_in_numeric
+
+    assert_equal 52, card_1.set_ones_in_numeric
+
+    card_2 = Card.new("10", "Hearts")
+    card_2.set_tens_in_numeric
+
+    assert_equal 103, card_2.set_ones_in_numeric
+  end
+
+  def test_it_sets_numeric
+    card_1 = Card.new("5", "Diamonds")
+
+    assert_equal 52, card_1.set_numeric
+
+    card_2 = Card.new("10", "Hearts")
+
+    assert_equal 103, card_2.set_numeric
+  end
 end
